769658e944d590861b508462fa700eb35f7824ce
leee1846 <leee1846@gmail.com>
2021-05-02 18:36:55 +0900

    test: 첫 지도화면 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+declare global {
+  interface Window {
+    naver: any;
+  }
+}
+
+const { naver } = window;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const mapOptions = {
+    center: new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 10,
+  };
+
+  const map = new naver.maps.Map("map", mapOptions);
+  return <div id='map' style={{ width: "100%", height: "500px" }} />;
 }
 
 export default App;
